796aca38fa15008ffcaf3bcf4b03483d71969c32
Henk Reder <henk@anestuary.com>
2014-10-16 15:11:07 -0400

    validated email address, added tests to author model

--- app/models/author.rb
+++ app/models/author.rb
@@ -6,11 +6,11 @@ class Author < ActiveRecord::Base
   has_many :projects, :through => :portfolio
 
   # Validations #
-  validates :first_name, length: {minimum: 2}
+  validates :first_name, presence: {message: "is not present"}
   # validates_format_of :first_name, :with => /^[^0-9`!@#\A%\z&*+_=]+$/
-  validates :last_name, length: {minimum: 2}
+  validates :last_name, presence: {message: "is not present"}
   # validates_format_of :last_name, :with => /^[^0-9`!@#\A%\z&*+_=]+$/
-  validates :username, length: {minimum: 6}
-  validates(:email, presence: true)
-  validates(:email, confirmation: true)
+  validates :username, length: {minimum: 2}
+  validates(:email, presence: {message: "is not present"})
+  validates_format_of :email, with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
 end
--- test/models/author_test.rb
+++ test/models/author_test.rb
@@ -1,7 +1,42 @@
 require 'test_helper'
 
 class AuthorTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+   test "create an author" do
+     a = Author.new first_name: "bob",
+                    last_name: "sagget",
+                    username: "whataguy",
+                    email: "whataguy@test.ts"
+     assert a.save, a.errors.full_messages
+   end
+
+   test "First name is not present" do
+     a = Author.new
+     refute a.save, 'First name is not present'
+     assert a.errors.full_messages.include?('First name is not present')
+   end
+
+   test "Last name is not present" do
+     a = Author.new
+     refute a.save, 'Last name is not present'
+     assert a.errors.full_messages.include?('Last name is not present')
+   end
+
+   test "Username is too short (minimum is 2 characters)" do
+     a = Author.new
+     refute a.save, 'Username is too short (minimum is 2 characters)'
+     assert a.errors.full_messages.include?('Username is too short (minimum is 2 characters)')
+   end
+
+   test "Email is not present" do
+     a = Author.new
+     refute a.save, 'Email is not present'
+     assert a.errors.full_messages.include?('Email is not present')
+   end
+
+   test "Email is invalid" do
+     a = Author.new(email:"hi")
+     refute a.save, 'Email is invalid'
+     assert a.errors.full_messages.include?('Email is invalid')
+   end
+
 end
